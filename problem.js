b79fe39c193de3af641d3f6b5b6942cd3653860d
LetPritom <pritom.royjess@gmail.com>
2026-01-20 15:49:45 +0600

    added my 9 problems

--- problem.js
+++ problem.js
@@ -6,7 +6,10 @@ function reverseString(str) {
     return reverse
 }
 
-reverseString('hello')
+
+
+const revCheck = reverseString('hello')
+console.log(revCheck)
 
 
 // 2: count vowels
@@ -36,7 +39,7 @@ const value = isPalindrome('vasi')
 console.log(value)
 
 
-// find max number
+// 4: find max number
 
 function findMaxNumber(arr) {
   let max = arr[0];
@@ -52,3 +55,92 @@ function findMaxNumber(arr) {
 
 const maxValue = findMaxNumber([1, 2, 9 , 6])
 console.log(maxValue)
+
+//5: remove duplicate function 
+
+function removeDuplicate (arr) {
+    let result = [] ;
+
+    for(i of arr) {
+        if(!result.includes(i))
+            result.push(i);
+    }
+
+    return result;
+}
+
+const check = removeDuplicate([1,2,1,3,3,3,4,1])
+console.log(check)
+
+
+// 6 sum function 
+
+function sumArr(arr) {
+  let sum = 0;
+
+  for (let num of arr) {
+    sum = sum + num;
+  }
+
+  return sum;
+}
+
+const total = sumArr([1,2,5,87,4,6])
+console.log(total)
+
+
+// 7 even number of array 
+
+function evenNumbersOfArray(array) {
+    const result = []
+
+    for( even of array) {
+        if(even % 2===0) {
+            result.push(even)
+        }
+    }
+
+    return result
+
+}
+
+const pushEve =  evenNumbersOfArray([2,4,5,6,47,52])
+console.log(pushEve)
+
+
+// 8 factorial number
+
+function factorialCalculation(num) {
+  let result = 1;
+
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
+}
+
+const fact = factorialCalculation(10);
+console.log(fact)
+
+
+// ping pong print 
+
+function pingPongPrint(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+const ping = pingPongPrint(20)
+console.log(20)
+
+
